a9d04dd8bfb27aa2589f0dfe921505a3efdc914f
zahodinikola <zahodinikola@gmail.com>
2017-02-27 21:27:53 +0200

    some added

--- js/code.js
+++ js/code.js
@@ -1,2 +1,23 @@
 // javaScript code
-alert ("wwwow");
+//alert ("wwwow");
+5;
+7;
+5.3;
+432;
+34, 43, 324.525;
+
+"this is a strimng";
+"this is a second string";
+"this is a \"spetial\" string";
+
+var myValue = 5;
+var mySecondValue = 7 + 9;
+var myThirdValue = myValue + mySecondValue;
+//alert(myThirdValue);
+
+var myName = "Jessy";
+var myLastName = "Liberty";
+var myFullName = myName + " " + myLastName;
+//alert(myFullName);
+
+var val = 5 + 9;
